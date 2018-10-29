b6560b2d908eb562130898d596f0a85c82784718
Mikkel P <mikkel-pieler@hotmail.com>
2018-10-29 11:12:40 +0100

    Second Commit

--- src/Run.java
+++ src/Run.java
@@ -1,7 +1,18 @@
+import Interfaces.Iterator;
+import Iterator.TaskRepository;
+
 public class Run
 {
     public static void main(String[] args)
     {
+        TaskRepository taskRepository = new TaskRepository();
 
+        taskRepository.addTask("Fyr op for radioen");
+        taskRepository.addTask("Start gravkoen");
+        taskRepository.addTask("Byg mur");
+        taskRepository.addTask("Fjern mur der er sat forkert op");
+
+
+        taskRepository.getIterator().print();
     }
 }
